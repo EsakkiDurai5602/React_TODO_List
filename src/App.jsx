0cd3a20049fabb99bd4e30c612072f2ff6718f4c
Esakki Durai <esakkidurai5600@gmail.com>
2026-04-03 18:04:25 +0530

    Fix missing index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import AddTODO from "./components/AddTODO";
-import Carhandle from "./components/Carhandle";
-import Colorpicker from "./components/Colorpicker";
-import Counter from "./components/counter";
 
 function App() {
 
